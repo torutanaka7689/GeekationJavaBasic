3c1ddac895b87d9dc039ae2ceb15e70704130005
toru tanaka <t.tanaka.cingroup2012@gmail.com>
2025-01-19 18:51:52 +0900

    ディレクトリの整理

--- src/basicSyntax/main.java
+++ src/basicSyntax/main.java
@@ -1,9 +1,10 @@
+package basicSyntax;
 
-public class Main {
+public class main {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("Hello Geekation");
+
 	}
 
 }
